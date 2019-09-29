a2dcb84c727f9ec91bc2f0fb53a7fadfff5c37ac
Wesley Henrique Batista Nunes <wesley.rn2@gmail.com>
2019-09-29 19:25:06 -0300

    Initial commit

---
+++ PSO/Main.py
@@ -0,0 +1,56 @@
+from PSO.PSO_basico import pso_basico
+from PSO.PSO_constricao import pso_constricao
+from PSO.PSO_inercia import pso_inercia
+import matplotlib.pyplot as plt
+import statistics
+
+
+def grafico(plot_data):
+    plt.plot(plot_data[0], label="PSO básico")
+    plt.plot(plot_data[1], linewidth=3, linestyle='dashed', label="PSO com fator de constrição", markersize=2)
+    plt.plot(plot_data[2], label="PSO com fator de inércia", markersize=12)
+    plt.title("Comparação entre PSO")
+    plt.legend()
+    plt.show()
+
+def main():
+    plot_data_basico = []
+    plot_data_constricao = []
+    plot_data_inercia = []
+
+    for i in range(5):
+        result_basico = pso_basico()
+        plot_data_basico.append(result_basico.fitness.values[0])
+
+    for i in range(5):
+        result_constricao = pso_constricao()
+        plot_data_constricao.append(result_constricao.fitness.values[0])
+
+    for i in range(5):
+        result_inercia = pso_inercia()
+        plot_data_inercia.append(result_inercia.fitness.values[0])
+
+    data = []
+    data.append(plot_data_basico)
+    data.append(plot_data_constricao)
+    data.append(plot_data_inercia)
+
+    print("Dados PSO básico")
+    print("Media: " + str(statistics.mean(plot_data_basico)))
+    print("Mediana: " + str(statistics.median(plot_data_basico)))
+    print("Desvio Padrão: " + str(statistics.stdev(plot_data_basico, None)))
+    print()
+    print("Dados PSO com fator de constrição")
+    print("Media: " + str(statistics.mean(plot_data_constricao)))
+    print("Mediana: " + str(statistics.median(plot_data_constricao)))
+    print("Desvio Padrão: " + str(statistics.stdev(plot_data_constricao, None)))
+    print()
+    print("Dados PSO com fator de inércia")
+    print("Media: " + str(statistics.mean(plot_data_inercia)))
+    print("Mediana: " + str(statistics.median(plot_data_inercia)))
+    print("Desvio Padrão: " + str(statistics.stdev(plot_data_inercia, None)))
+    grafico(data)
+
+
+if __name__ == "__main__":
+    main()
---
+++ PSO/PSO_inercia.py
@@ -0,0 +1,82 @@
+import operator
+import random
+
+import numpy
+import math
+
+from deap import base
+from deap import creator
+from deap import tools
+
+creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+creator.create("Particle", list, fitness=creator.FitnessMin, speed=list,
+               smin=None, smax=None, best=None)
+
+
+
+def generate(size, pmin, pmax, smin, smax):
+    part = creator.Particle(random.uniform(pmin, pmax) for _ in range(size))
+    part.speed = [random.uniform(smin, smax) for _ in range(size)]
+    part.smin = smin
+    part.smax = smax
+    return part
+
+
+def matyas_function(x):
+    return 0.26*((x[0]**2) + (x[1]**2)) - .48*x[0]*x[1],
+
+
+def updateParticle(part, best,fator_de_inercia, phi1, phi2):
+    u1 = (random.uniform(0, phi1) for _ in range(len(part)))
+    u2 = (random.uniform(0, phi2) for _ in range(len(part)))
+    v_u1 = map(operator.mul, u1, map(operator.sub, part.best, part))
+    v_u2 = map(operator.mul, u2, map(operator.sub, best, part))
+    part.speed = list(map(lambda x: x * fator_de_inercia, part.speed))
+    part.speed = list(map(operator.add, part.speed, map(operator.add, v_u1, v_u2)))
+    for i, speed in enumerate(part.speed):
+        if abs(speed) < part.smin:
+            part.speed[i] = math.copysign(part.smin, speed)
+        elif abs(speed) > part.smax:
+            part.speed[i] = math.copysign(part.smax, speed)
+    part[:] = list(map(operator.add, part, part.speed))
+
+
+toolbox = base.Toolbox()
+toolbox.register("particle", generate, size=2, pmin=-10, pmax=10, smin=-3, smax=3)
+toolbox.register("population", tools.initRepeat, list, toolbox.particle)
+toolbox.register("update", updateParticle, phi1=2.0, phi2=2.0)
+toolbox.register("evaluate", matyas_function)
+
+
+def pso_inercia():
+    pop = toolbox.population(n=50)
+    stats = tools.Statistics(lambda ind: ind.fitness.values)
+    stats.register("avg", numpy.mean)
+    stats.register("std", numpy.std)
+    stats.register("min", numpy.min)
+    stats.register("max", numpy.max)
+
+    logbook = tools.Logbook()
+    logbook.header = ["gen", "evals"] + stats.fields
+
+    GEN = 1000
+    best = None
+
+    for g in range(GEN):
+        fator_de_inercia = 0.9 - g * (0.5 / GEN)
+        for part in pop:
+            part.fitness.values = toolbox.evaluate(part)
+            if not part.best or part.best.fitness < part.fitness:
+                part.best = creator.Particle(part)
+                part.best.fitness.values = part.fitness.values
+            if not best or best.fitness < part.fitness:
+                best = creator.Particle(part)
+                best.fitness.values = part.fitness.values
+        for part in pop:
+            toolbox.update(part, best, fator_de_inercia)
+
+        # Gather all the fitnesses in one list and print the stats
+        logbook.record(gen=g, evals=len(pop), **stats.compile(pop))
+        # print(logbook.stream)
+    # print(best.fitness.values)
+    return best
