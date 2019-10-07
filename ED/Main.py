cdd9fbed3635ecc52e5e5752fa1c192a0febf348
Wesley Henrique Batista Nunes <wesley.rn2@gmail.com>
2019-10-07 00:24:37 -0300

    ED_Aprend_opo implemented

--- ED/Main.py
+++ ED/Main.py
@@ -1,6 +1,7 @@
 from ed_basico import *
 from ed_rand import *
 from ed_best import *
+from ed_basico_aprend_opo import *
 import matplotlib.pyplot as plt
 import statistics
 
@@ -8,7 +9,8 @@ import statistics
 def grafico(plot_data):
     plt.plot(plot_data[0], label="ED básico")
     plt.plot(plot_data[1], label="ED Rand")
-    plt.plot(plot_data[2], label="ED best")
+    plt.plot(plot_data[2], label="ED aprendizado oposição")
+    plt.plot(plot_data[3], label="ED best")
     plt.legend()
     plt.title("Comparação entre ED")
     plt.savefig("chart.svg")
@@ -18,17 +20,20 @@ def main():
     plot_basico = []
     plot_rand = []
     plot_best = []
+    plot_basico_aprend = []
     for i in range(100):
-        plot_basico.append(ed_basico())
+        plot_basico.append(ed_basico_aprend(1))
 
     for i in range(100):
         plot_rand.append(ed_rand())
 
     for i in range(100):
-        print(i)
+        plot_basico_aprend.append(ed_basico_aprend(0.7))
+
+    for i in range(100):
         plot_best.append(ed_best())
 
-    data = [plot_basico, plot_rand, plot_best]
+    data = [plot_basico, plot_rand, plot_basico_aprend, plot_best]
 
     print("Dados ED básico")
     print("Media: " + str(statistics.mean(plot_basico)))
@@ -39,6 +44,11 @@ def main():
     print("Media: " + str(statistics.mean(plot_rand)))
     print("Mediana: " + str(statistics.median(plot_rand)))
     print("Desvio Padrão: " + str(statistics.stdev(plot_rand, None)))
+
+    print("Dados ED básico Aprendizado oposição")
+    print("Media: " + str(statistics.mean(plot_basico_aprend)))
+    print("Mediana: " + str(statistics.median(plot_basico_aprend)))
+    print("Desvio Padrão: " + str(statistics.stdev(plot_basico_aprend, None)))
 
     print("Dados ED Best")
     print("Media: " + str(statistics.mean(plot_best)))
