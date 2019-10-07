d60716cd01de8342fb3ca0c8a5afaad3e040cf7b
Wesley Henrique Batista Nunes <wesley.rn2@gmail.com>
2019-10-06 23:35:35 -0300

    ED_rand and ED_best implemented

--- ED/Main.py
+++ ED/Main.py
@@ -1,10 +1,14 @@
 from ed_basico import *
+from ed_rand import *
+from ed_best import *
 import matplotlib.pyplot as plt
 import statistics
 
 
 def grafico(plot_data):
     plt.plot(plot_data[0], label="ED básico")
+    plt.plot(plot_data[1], label="ED Rand")
+    plt.plot(plot_data[2], label="ED best")
     plt.legend()
     plt.title("Comparação entre ED")
     plt.savefig("chart.svg")
@@ -12,14 +16,35 @@ def grafico(plot_data):
 
 def main():
     plot_basico = []
-    for i in range(10):
+    plot_rand = []
+    plot_best = []
+    for i in range(100):
         plot_basico.append(ed_basico())
 
-    data = [plot_basico]
+    for i in range(100):
+        plot_rand.append(ed_rand())
+
+    for i in range(100):
+        print(i)
+        plot_best.append(ed_best())
+
+    data = [plot_basico, plot_rand, plot_best]
+
     print("Dados ED básico")
     print("Media: " + str(statistics.mean(plot_basico)))
     print("Mediana: " + str(statistics.median(plot_basico)))
     print("Desvio Padrão: " + str(statistics.stdev(plot_basico, None)))
+
+    print("Dados ED Rand")
+    print("Media: " + str(statistics.mean(plot_rand)))
+    print("Mediana: " + str(statistics.median(plot_rand)))
+    print("Desvio Padrão: " + str(statistics.stdev(plot_rand, None)))
+
+    print("Dados ED Best")
+    print("Media: " + str(statistics.mean(plot_best)))
+    print("Mediana: " + str(statistics.median(plot_best)))
+    print("Desvio Padrão: " + str(statistics.stdev(plot_best, None)))
+
     grafico(data)
 
 
---
+++ ED/ed_rand.py
@@ -0,0 +1,69 @@
+import random
+import array
+
+import numpy
+
+from deap import base
+from deap import creator
+from deap import tools
+
+# Problem dimension
+NDIM = 2
+
+creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+creator.create("Individual", array.array, typecode='d', fitness=creator.FitnessMin)
+
+
+def matyas_function(x):
+    return 0.26 * ((x[0] ** 2) + (x[1] ** 2)) - .48 * x[0] * x[1],
+
+
+toolbox = base.Toolbox()
+toolbox.register("attr_float", random.uniform, -10, 10)
+toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.attr_float, NDIM)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+toolbox.register("select", tools.selRandom, k=5)
+toolbox.register("evaluate", matyas_function)
+
+
+def ed_rand():
+    CR = 0.25
+    F = 1
+    MU = 300
+    NGEN = 200
+
+    pop = toolbox.population(n=MU);
+    hof = tools.HallOfFame(1)
+    stats = tools.Statistics(lambda ind: ind.fitness.values)
+    stats.register("avg", numpy.mean)
+    stats.register("std", numpy.std)
+    stats.register("min", numpy.min)
+    stats.register("max", numpy.max)
+
+    logbook = tools.Logbook()
+    logbook.header = "gen", "evals", "std", "min", "avg", "max"
+
+    # Evaluate the individuals
+    fitnesses = toolbox.map(toolbox.evaluate, pop)
+    for ind, fit in zip(pop, fitnesses):
+        ind.fitness.values = fit
+
+    record = stats.compile(pop)
+    logbook.record(gen=0, evals=len(pop), **record)
+
+    for g in range(1, NGEN):
+        for k, agent in enumerate(pop):
+            a, b, c, d, e = toolbox.select(pop)
+            y = toolbox.clone(agent)
+            index = random.randrange(NDIM)
+            for i, value in enumerate(agent):
+                if i == index or random.random() < CR:
+                    y[i] = a[i] + F * (b[i] - c[i]) + F * (d[i] - e[i])
+            y.fitness.values = toolbox.evaluate(y)
+            if y.fitness > agent.fitness:
+                pop[k] = y
+        hof.update(pop)
+        record = stats.compile(pop)
+        logbook.record(gen=g, evals=len(pop), **record)
+
+    return hof[0].fitness.values[0]
